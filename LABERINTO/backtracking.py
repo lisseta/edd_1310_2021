8edb75f04009461bd01e6f9e12d230172bf13505
Lizeth Evelyn Hernández Piedras <72099521+lisseta@users.noreply.github.com>
2020-12-03 13:30:24 -0600

    Add files via upload

--- LABERINTO/backtracking.py
+++ LABERINTO/backtracking.py
@@ -13,6 +13,42 @@ class LaberintoADT:
     def to_string(self):
         self.__laberinto.to_string()
 
+'''
+    Establece la entrada 'E', en la matriz, verificar limites
+    '''
+    def set_entrada( self, ren, col ):
+        #terminar la validacion de las coordenadas
+        self.__laberinto.set_item( ren, col, 'E')
+
+    '''
+    Establece la salida 'S', en la matriz, verificar limites periféricos
+    '''
+    def set_salida( self, ren, col ):
+        #terminar la validacion de las coordenadas
+        self.__laberinto.set_item( ren, col, 'S')
+
+    def es_salida( self, ren, col ):
+        return self.__laberinto.get_item( ren, col ) == 'S'
+    
+    def buscar_entrada( self ):
+        encontrado = False
+        for renglon in range(self.__laberinto.get_num_rows() ):
+            for columna in range(self.__laberinto.get_num_cols() ):   
+                if self.__laberinto.get_item(renglon, columna) == 'E':
+                    self.__camino.push( renglon, columna )
+                    encontrado = True
+        return encontrado
+
+    def set_previa( self, pos_previa):
+        self.__previa = pos_previa
+    
+    def get_preiva( self ):
+        return self.__previa
+    
+    def resolver_laberinto( self ):
+        #aplicar reglas
+        pass        
+
 
 
 #El siguiente codigo en COLAB
--- LABERINTO/prueba.py
+++ LABERINTO/prueba.py
@@ -2,3 +2,5 @@ from backtracking import LaberintoADT
 pasillos_inicial=((2,1), (2,2), (2,3), (2,4), (3,2), (4,2))
 lab=LaberintoADT(6,6, pasillos_inicial)
 lab.to_string()
+
+#def __init__(self,rens,cols,pasillos, entrada, salida):
